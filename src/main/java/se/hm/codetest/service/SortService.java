4dab615037debe214543cf0fdfc8d0578f3a0231
Karl Johan Andreasson <kalle.kaan@gmail.com>
2018-05-01 22:46:14 +0200

    Adding initial documentation of the SortService

--- src/main/java/se/hm/codetest/service/SortService.java
+++ src/main/java/se/hm/codetest/service/SortService.java
@@ -4,8 +4,22 @@ import se.hm.codetest.controller.dto.SortedDto;
 
 import java.util.List;
 
+/**
+ * Service to sort integers using Radix sort.
+ */
 public interface SortService {
+    /**
+     * Retrieves all the previous sorting results.
+     *
+     * @return the results from previous sorting
+     */
     List<SortedDto> getAll();
 
+    /**
+     * Sorts the integers in inputList and returns the sorted array along with meta data.
+     *
+     * @param inputList the integers to be sorted, they are assumed to be between 0 and 100 (not inclusive)
+     * @return the result of the sorting.
+     */
     SortedDto sortArray(List<Integer> inputList);
 }
